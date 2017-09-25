65a50675146073a7696275a3a9efff1a4cd8fae6
dujardin <antoinedujardin42@gmail.com>
2017-09-25 20:50:19 +0200

    added class to handle temp device/sensor

--- Client/harvester/harvester.py
+++ Client/harvester/harvester.py
@@ -1,16 +1,18 @@
+import sys
+import collections
 import platform  # system info
 import psutil  # hardware info
 import cpuinfo  # detailed CPU info
-#import pySMART  # hard drive smart info, requires admin
+# import pySMART  # hard drive smart info, requires admin. DOESNT FUCKING WORK
 import influxdb  # communication with influxdb
 
 
 # DATA COLLECTION
 class DiskInfo:
-    def __init__(self, info):  # separer tout
-        self.device = info[0]
-        self.mount = info[1]
-        self.file_system = info[2]
+    def __init__(self, device_, mount_, file_system_):  # separer tout
+        self.device = device_
+        self.mount = mount_
+        self.file_system = file_system_
 
         disk_usage_info = psutil.disk_usage(self.mount)
         self.space_total_B = disk_usage_info[0]
@@ -23,6 +25,82 @@ class DiskInfo:
         self.space_available_B = disk_usage_info[2]
         self.space_used_B = disk_usage_info[1]
         self.space_used_percent = disk_usage_info[3]
+
+
+class TemperatureSensor:
+    def __init__(self, name_, current_, high_, critical_):
+        self.name = name_
+        self.current = current_
+        self.high = high_
+        self.critical = critical_
+
+    def is_high(self):
+        if self.current >= self.high:
+            return True
+        return False
+
+    def is_critical(self):
+        if self.current >= self.critical:
+            return True
+        return False
+
+
+class TemperatureDevice:
+    name_to_device = collections.defaultdict(str)
+    name_to_device_list = [
+        ["coretemp", "CPU"],
+        ["acpitz", "motherboard"]
+    ]
+
+    def __init__(self, name_, sensors_):
+        for k, v in self.name_to_device_list:
+            self.name_to_device[k] = v
+
+        self.name = name_
+        self.type = self.name_to_device[self.name]
+        self.sensors = []
+        for curr_sensor in sensors_:
+            self.sensors.append(TemperatureSensor(curr_sensor[0], curr_sensor[1], curr_sensor[2], curr_sensor[3]))
+
+    def is_high(self):
+        high = 0
+        for curr_sensor in self.sensors:
+            if curr_sensor.is_high():
+                high += 1
+        return high
+
+    def is_critical(self):
+        critical = 0
+        for curr_sensor in self.sensors:
+            if curr_sensor.is_critical():
+                critical += 1
+        return critical
+
+    def highest(self):
+        highest = -274
+        highest_name = ""
+        for curr_sensor in self.sensors:
+            if curr_sensor.current > highest:
+                highest = curr_sensor.current
+                highest_name = curr_sensor.name
+        return [highest, highest_name]
+
+    def lowest(self):
+        lowest = sys.maxsize
+        lowest_name = ""
+        for curr_sensor in self.sensors:
+            if curr_sensor.current < lowest:
+                lowest = curr_sensor.current
+                lowest_name = curr_sensor.name
+        return [lowest, lowest_name]
+
+    def average(self):
+        total = 0
+        number = 0
+        for curr_sensor in self.sensors:
+            total += curr_sensor.current
+            number += 1
+        return total/number
 
 
 # Platform
@@ -66,12 +144,19 @@ disks_info = psutil.disk_partitions()
 disks = []
 for curr_disk in disks_info:  # ignore partitions under 1 GB?
     if curr_disk[2] != "":
-        disks.append(DiskInfo(curr_disk))
+        disks.append(DiskInfo(curr_disk[0], curr_disk[1], curr_disk[2]))
 
 # Sensors
-temp_info = None
+temp_devices = []
+temp_unavailable = False
 if hasattr(psutil, "sensors_temperatures"):
     temp_info = psutil.sensors_temperatures()
+    if temp_info is not None:
+        for device_name, sensors in temp_info.items():
+            temp_devices.append(TemperatureDevice(device_name, sensors))
+    else:
+        temp_unavailable = True
+
 fans_info = None
 if hasattr(psutil, "sensors_fans"):
     fans_info = psutil.sensors_fans()
@@ -96,7 +181,7 @@ json_body = [
     }
 ]
 client = influxdb.InfluxDBClient("192.168.1.33", 8086, "admineasy-client", "1337" "admineasy")
-#success = client.write_points(json_body)
+# success = client.write_points(json_body)
 
 
 # DISPLAY
@@ -122,24 +207,35 @@ print('\n' + "Disks")
 for disk in disks:
     disk_text = disk.device
     if disk.mount != disk.device:
-        disk_text += " (" + disk.mount + ")"
+        disk_text += "  (" + disk.mount + ")"
     disk_text += "    File System: " + disk.file_system
     disk_text += "    Total space: " + str(round(disk.space_total_B/1000000000, 1)) + "GB"
     disk_text += "  Used: " + str(disk.space_used_percent) + "%"
     print(disk_text)
 
 print('\n' + "Temperature")
-if temp_info is None:
-    print("Temperature information unavailable")
-else:
-    if len(temp_info) == 0:
-        print("No temperature sensor detected")
+if len(temp_devices) == 0:
+    if temp_unavailable:
+        print("Temperature information unavailable on this system")
     else:
-        for device_name, device_info in temp_info.items():
-            device_text = device_name + ":  "
-            for sensor in device_info:
-                device_text += "  " + sensor[0] + ": " + str(sensor[1]) + "C"
-            print(device_text)
+        print("No temperature sensor was detected")
+else:
+    for device in temp_devices:
+        device_text = ""
+
+        if device.type != "":
+            device_text += device.type
+        else:
+            device_text += device.name
+
+        device_text += "    Average: " + str(round(device.average(), 1)) + "C"
+
+        if device.is_critical():
+            device_text += "  TEMPERATURE CRITICAL"
+        elif device.is_high():
+            device_text += "  Temperature high"
+
+        print(device_text)
 
 print('\n' + "Fans")
 if fans_info is None:
