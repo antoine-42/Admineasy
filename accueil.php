b3b3f6599a17900b6bce73bc6816d8534fc3c23b
Da Costa <mel-dacosta@hotmail.fr>
2018-02-14 12:31:08 +0000

    r

--- accueil.php
+++ accueil.php
@@ -26,7 +26,7 @@
 
 // influx
 
-
+/*
 $client = new \crodas\InfluxPHP\Client(
    "10.8.0.1",  //host
    8086,   //port
@@ -44,5 +44,35 @@ foreach ($db->query("SELECT * FROM ram") as $row) {
 
 	var_dump($row, $row->time);
 }
+*/
+
+// postgre
+///psql -d admineasy -U admineasy_client -h 10.8.0.1
+
+
+// Connexion, sélection de la base de données
+$dbconn = pg_connect("host=10.8.0.1 dbname=admineasy user=admineasy-client password=1337")
+    or die('Connexion impossible : ' . pg_last_error());
+
+// Exécution de la requête SQL
+$query = 'SELECT * FROM Machines';
+$result = pg_query($query) or die('Échec de la requête : ' . pg_last_error());
+
+// Affichage des résultats en HTML
+echo "<table>\n";
+while ($line = pg_fetch_array($result, null, PGSQL_ASSOC)) {
+    echo "\t<tr>\n";
+    foreach ($line as $col_value) {
+        echo "\t\t<td>$col_value</td>\n";
+    }
+    echo "\t</tr>\n";
+}
+echo "</table>\n";
+
+// Libère le résultat
+pg_free_result($result);
+
+// Ferme la connexion
+pg_close($dbconn);
 
 ?>
