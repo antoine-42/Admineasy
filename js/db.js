57d9b5a3fb205d7f608c8d693c6c6ba111b1ed78
Da Costa <mel-dacosta@hotmail.fr>
2018-02-15 13:57:58 +0100

    postgre

--- js/db.js
+++ js/db.js
@@ -1,4 +1,4 @@
-var pg = require('/home/invite/js_node/node_modules/pg') ;
+/*var pg = require('/home/invite/js_node/node_modules/pg') ;
 var conString = "postres://admineasy_client:1337@10.8.0.1:5432/admineasy" ;
 
 var client = new pg.Client(conString) ;
@@ -16,3 +16,14 @@ query.on('end', function(end)
 				{
 					client.end() ;
 				}) ;
+
+*/
+var sys = require("sys"),
+my_http = require("http");
+my_http.createServer(function(request,response){
+  sys.puts("I got kicked");
+  response.writeHeader(200, {"Content-Type": "text/plain"});
+  response.write("Hello World");
+  response.end();
+}).listen(8080);
+sys.puts("Server Running on 8080"); 
