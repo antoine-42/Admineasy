230595c7fd3eeecdab6032a1f66d5c62259d2b96
Da Costa <mel-dacosta@hotmail.fr>
2018-02-19 09:37:18 +0000

    e

--- index_node_serveur.js
+++ index_node_serveur.js
@@ -52,7 +52,7 @@ var reaction = function(req, res)
 							
 						/**Affichage**/
 						
-						res.writeHead(200, {"Content-Type": "text/html"}) ; 	//Code de retour indiquant que la page fonctionne (404 --> non trouvée...), type de retour(html, image...)
+						res.writeHead(200, {"Content-Type": "text"}) ; 	//Code de retour indiquant que la page fonctionne (404 --> non trouvée...), type de retour(html, image...)
 						//Prépare le code HTML
 						codeHtml = '<!DOCTYPE html>'+
 						'<html>'+
