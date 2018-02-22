60e61d18e1d6d7abd25210eecd7b9b87ae43e10b
Da Costa <mel-dacosta@hotmail.fr>
2018-02-22 13:49:49 +0000

    modif

---
+++ js/Methode.js
@@ -0,0 +1,35 @@
+var Methode={
+	function searchIP(){
+		var requete= creerRequete();
+		var arg=document.getElementById("ip").value;
+		var url="http://nailyk.ddns.net:54823/machine?ip="+arg ;
+		requete.open("POST", url, true);
+		requete.onreadystatechange=function(){
+			afficherResultat(requete);
+		}
+
+		requete.send();
+		}
+
+		function search(){
+		var requete= creerRequete();
+		var url="http://nailyk.ddns.net:54823/listmachine" ;
+		requete.open("POST", url, true);
+		requete.onreadystatechange=function(){
+			afficherResultat(requete);
+		}
+
+		requete.send();
+		}
+
+
+	 function afficherResultat(requete){
+	 		console.log("afficherResultat");
+		if(requete.readyState==4 && requete.status==200){
+			console.log("afficherIIIFFFF");
+			elem=document.getElementById("texte");
+			console.log("RESP: "+requete.responseText);
+			elem.innerHTML=requete.responseText;
+		}
+	}
+}
