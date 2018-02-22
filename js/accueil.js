60828210b0909a521f7af16806cbfcfc2237df25
Da Costa <mel-dacosta@hotmail.fr>
2018-02-22 14:10:52 +0000

    modif

--- js/accueil.js
+++ js/accueil.js
@@ -1,5 +1,3 @@
-
-
 var Accueil = {
 	nbAlertes: 0,
 
--- js/requete.js
+++ js/requete.js
@@ -1,79 +1,21 @@
 function creerRequete(){
-
 	console.log("creerRequete");
-
-	
-
     var xhr = null;
-
-
-
-    
-
-
-
     if (window.XMLHttpRequest || window.ActiveXObject) {
-
-
-
         if (window.ActiveXObject) {
-
-
-
             try {
-
-
-
                 xhr = new ActiveXObject("Msxml2.XMLHTTP");
-
-
-
             } catch(e) {
-
-
-
                 xhr = new ActiveXObject("Microsoft.XMLHTTP");
-
-
-
             }
-
-
-
         } else {
-
-
-
             xhr = new XMLHttpRequest(); 
-
-
-
         }
-
-
-
     } else {
-
-
-
         alert("Votre navigateur ne supporte pas l'objet XMLHTTPRequest...");
-
-
-
         return null;
-
-
-
     }
-
-
-
     console.log("xhr "+xhr);
-
-
-
     return xhr;
-
-
 
 }
