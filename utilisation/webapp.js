71b01d90dd9f7ee82268bfa8ead71d4912aa610f
Antoine <antoine.dujardin@epita.fr>
2018-03-04 16:34:10 +0100

    plusieurs sources, date propre, autorefresh

--- utilisation/webapp.js
+++ utilisation/webapp.js
@@ -19,46 +19,36 @@ app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x
 app.use(bodyParser.json()); // for parsing application/json
 
 app.get('/', function (req, res) {
-  res.redirect('/index.html')
+  res.redirect('/index.html');
 })
 
-app.post('/time_cpu', upload.array(), function(req, res, next) {
-    // period is expressed in seconds to ease updates
-    var limit=(Date.now()-req.body.cpu_period*1000)*1e6;
-    var db_rq = "select time,used from cpu where machine='"+req.body.cpu_machine+"' and time>"+limit+" order by time";
+var types = {
+    "cpu": {"usage": {"influx": "used"}, "freq": {"influx": "freq"}},
+    "ram": {"usage": {"influx": "used_percent"}},
+    "swap": {"usage": {"influx": "used_percent"}},
+    "partition": {"usage_full": {"influx": "partition,total,used_percent"}}
+};
 
-    influx.query(db_rq)
-        .then(results => { 
-                 res.json(results);
-              }
-             )
-});
-// app.post('/time_cpu', upload.array(), function(req, res, next) {
-/*
-app.get('/time_cpu', bodyParser.json(deflate=true, type='* / *'), function(req, res, next) {
-    console.log('received '+req.body.cpu_machine);
-    limit=Date.now()-req.body.cpu_period*3600000;
-    influx.query("select time,used from cpu where machine='"+req.body.cpu_machine+"' and time>'+limit+' order by time")
-        .then(results => { 
-                 res.json(results);
-              }
-             )
+for(var type in types){
+    for(var mesure in types[type]){
+        var id = type + "_" + mesure;
 
+        app.post('/' + id, upload.array(), function(req, res, next) {
+            // period is expressed in seconds to ease updates
+            var param = req.url.substr(1).split("_");
+            var limit=(Date.now()-req.body.cpu_period*1000)*1e6;
+            var db_rq = "select time," + types[param[0]][param[1]]["influx"] + " from " + param[0] + " where machine='"+req.body.cpu_machine+"' and time>"+limit+" order by time";
 
-//    res.send('Hello '+req.body.name+' from '+req.body.city);
-})
-*/
-
-const parser=express.json()
-
-app.get('/time_cpu', function(req, res) {
-    console.log(req.cpu_period);
-    var myjs=parser(req.body);
-    console.log(myjs);
-    res.json({rep:'hello'});
-})
+            influx.query(db_rq)
+                .then(results => {
+                    res.json(results);
+                }
+            )
+        });
+    }
+}
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+  console.log('app listening on port 3000!');
 })
 
